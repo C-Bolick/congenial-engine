ff07b2ae3068cd3fbfa5e56c3659e40e23233a0e
C-Bolick <134232955+C-Bolick@users.noreply.github.com>
2023-08-02 18:34:03 -0400

    Add files via upload

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,8 +3,6 @@ const { Category, Product } = require('../../models');
 const ProductData = {Category, Product}
 
 // The `/api/categories` endpoint
-
-router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
   router.get('/', async (req, res) => {
@@ -12,8 +10,7 @@ router.get('/', (req, res) => {
       const CategoryData = await Category.findAll({
         include: [
           {
-            model: Category,
-            attributes: ['id', 'category_name'],
+            model: Product
           },
         ],
       });
@@ -23,13 +20,12 @@ router.get('/', (req, res) => {
       res.status(500).json(err);
     }
   });
-});
 
   // find one category by its `id` value
   // be sure to include its associated Products
-  router.get('/category/:id', async (req, res) => {
+  router.get('/:id', async (req, res) => {
       try {
-        const categoryData = await Category.findByPk(req.params.id);
+        const categoryData = await Category.findByPk(req.params.id, {include: [Product]});
         if (!categoryData) {
           res.status(404).json({ message: 'No category with this id!' });
           return;
@@ -58,7 +54,7 @@ router.put('/:id', async (req, res) => {
         id: req.params.id,
       },
     });
-    if (!categoryData[0]) {
+    if (!categoryData) {
       res.status(404).json({ message: 'No category with this id!' });
       return;
     }
--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -4,27 +4,39 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
-});
+  router.get('/', async (req, res) => {
+    try {
+      const CategoryData = await Product.findAll({
+        include: [
+          {
+            model: Category
+          }, 
+          {
+            model: Tag, through:ProductTag
+          }
+        ],
+      });
+      res.status(200).json(CategoryData)
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
 
 // get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  
 });
 
 // create new product
 router.post('/', (req, res) => {
-  /* req.body should look like this...
     {
       product_name: "Basketball",
       price: 200.00,
       stock: 3,
       tagIds: [1, 2, 3, 4]
     }
-  */
+
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
